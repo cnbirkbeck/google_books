55a656a30d210427ce6a7e6043c66701c3ad175a
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-14 13:35:00 -0700

    initial commit added mongo to server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to mongoose DB
+const mongoose = require("mongoose");
+const mongoURL = process.env.MONGODB_URI || "mongodb://localhost/googlebooks"
+mongoose.connect(mongoURL, {useNewUrlParser: true})
+  .then(()=>{
+    console.log("🗄 ==> Successfully connected to mongoDB.");
+  })
+  .catch((err) => {
+    console.log(`Error connecting to mongoDB: ${err}`);
+  });
+  
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
