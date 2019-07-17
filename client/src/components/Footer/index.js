3e717773a29dd6351e5fcacd47e561c7800c3c6a
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-17 11:58:08 -0700

    updated components , added footer, reworked grid, switched header for jumbotron

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>React Google Book Search</p>
+			<p> C.Birkbeck 2019 </p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
