20d6d52e73ad6659a5fe17d602e44b8ec059a625
CBirkbeck <cnbirkbeck@gmail.com>
2019-07-17 00:24:02 -0700

    updated db model and controller, fixing api error

--- models/Book.js
+++ models/Book.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const BookSchema = new Schema({
-    title: String,
-    authors: [String],
-    description: String,
-    img: {
-        type: {String},
-    },
-    link: {
-        type: String,
-    }
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  authors: [{type: String, required: true}],
+  description: String,
+  image: {type: String, trim: true},
+  link: {type: String, trim: true},
+  date: { type: Date, default: Date.now }
 });
 
-const Book = mongoose.model("Book", BookSchema);
+const Book = mongoose.model("Book", bookSchema);
+
 module.exports = Book;
